a95c556c2215b5aa4d69e2a439457ecb0bfc2644
Daniel Alonso <dalonso@caser.es>
2016-06-29 15:05:57 +0200

    first commit

---
+++ src/app/index.config.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularFormly')
+    .config(config);
+
+  /** @ngInject */
+  function config($logProvider) {
+    // Enable log
+    $logProvider.debugEnabled(true);
+
+    // Set options third-party lib
+ 
+  }
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,85 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularFormly')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, webDevTec) {
+    var vm = this;
+
+    vm.awesomeThings = [];
+    vm.classAnimation = '';
+    vm.creationDate = 1467200083570;
+   vm.user = {};
+vm.options = {};
+  vm.userFields = [
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Email address',
+        placeholder: 'Enter email'
+      }
+    },
+    {
+      key: 'password',
+      type: 'input',
+      templateOptions: {
+        type: 'password',
+        label: 'Password',
+        placeholder: 'Password'
+      },
+	  
+	  expressionProperties: {
+          'templateOptions.disabled': '!user.email'
+        }
+      
+    },
+    {
+      key: 'file',
+      type: 'input',
+      templateOptions: {
+		type: 'file',
+        label: 'File input',
+        description: 'Example block-level help text here',
+        url: 'https://example.com/upload'
+      }
+    },
+    {
+      key: 'checked',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Check me out'
+      }
+    }
+  ];
+  
+  vm.onSubmit = onSubmit;
+ 
+  
+ 
+    activate();
+
+    function activate() {
+      getWebDevTec();
+      $timeout(function() {
+        vm.classAnimation = 'rubberBand';
+      }, 4000);
+    }
+
+  
+ function onSubmit() {
+    
+  }
+    function getWebDevTec() {
+      vm.awesomeThings = webDevTec.getTec();
+
+      angular.forEach(vm.awesomeThings, function(awesomeThing) {
+        awesomeThing.rank = Math.random();
+      });
+    }
+  }
+})();
